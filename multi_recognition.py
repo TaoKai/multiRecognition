39479052296bf843a83e0605b99cb1f441cb4f18
kai.tao <kai.tao@inveno.com>
2021-06-03 16:13:03 +0800

    sec

--- multi_recognition.py
+++ multi_recognition.py
@@ -6,6 +6,7 @@ import random
 sys.path.append('./CosFace_pytorch')
 from mtcnn import DetectFace, drawAll
 from cosface_pred import get_img_feature, get_distance
+import math
 
 detectFace = DetectFace()
 
@@ -29,6 +30,38 @@ def get_frame_id_map(feat_dict):
                 fid_map[fid].append((c, b, k))
     return fid_map
 
+def get_box_iou(b0, b1):
+    def compute_iou(rec1, rec2):
+        """
+        computing IoU
+        :param rec1: (y0, x0, y1, x1), which reflects
+                (top, left, bottom, right)
+        :param rec2: (y0, x0, y1, x1)
+        :return: scala value of IoU
+        """
+        # computing area of each rectangles
+        S_rec1 = (rec1[2] - rec1[0]) * (rec1[3] - rec1[1])
+        S_rec2 = (rec2[2] - rec2[0]) * (rec2[3] - rec2[1])
+    
+        # computing the sum_area
+        sum_area = S_rec1 + S_rec2
+    
+        # find the each edge of intersect rectangle
+        left_line = max(rec1[1], rec2[1])
+        right_line = min(rec1[3], rec2[3])
+        top_line = max(rec1[0], rec2[0])
+        bottom_line = min(rec1[2], rec2[2])
+    
+        # judge if there is an intersect
+        if left_line >= right_line or top_line >= bottom_line:
+            return 0
+        else:
+            intersect = (right_line - left_line) * (bottom_line - top_line)
+            return (intersect / (sum_area - intersect))*1.0
+    x00, y00, x01, y01, _ = b0
+    x10, y10, x11, y11, _ = b1
+    return compute_iou((y00,x00,y01,x01), (y10,x10,y11,x11))
+
 def merge_features_one_round(feat_dict):
     merge_flag = False
     for k, v in feat_dict.items():
@@ -41,12 +74,12 @@ def merge_features_one_round(feat_dict):
             random.shuffle(feats1)
             feats0 = feats0[:20]
             feats1 = feats1[:10]
-            pass_cnt = min(3, len(feats0), len(feats1))
+            pass_cnt = min(8, len(feats0), len(feats1))
             is_feat_cnt = 0
             for f0, _, _ in feats0:
                 for f1, _, _ in feats1:
                     dist = get_distance(f0, f1)
-                    if dist>0.28:
+                    if dist>0.5:
                         is_feat_cnt += 1
                     if is_feat_cnt==pass_cnt:
                         break
@@ -109,13 +142,17 @@ def video_analysis(path):
                     fc = get_img_feature(c)
                     best_id = -1
                     best_dist = -1
+                    best_iou = -1
                     for key in last_feats.keys():
                         fl = last_feats[key][-1][0]
+                        bl = last_feats[key][-1][1]
+                        iou = get_box_iou(bl, b)
                         dist = get_distance(fl, fc)
                         if dist>best_dist:
                             best_dist = dist
                             best_id = key
-                    if best_dist>=0.5:
+                            best_iou = iou
+                    if best_dist>=0.7 or (best_dist>0.35 and best_iou>0.7):
                         last_feats[best_id].append((fc, b, frame_id))
                     else:
                         last_feats[feat_id] = [(fc, b, frame_id)]
@@ -140,4 +177,4 @@ def video_analysis(path):
         new_frames.append(frame)
     saveVideo('tmp.mp4', new_frames, frate)
 
-video_analysis('videos/sample.mp4')
+video_analysis('videos/sample5.mp4')
